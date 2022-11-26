cea22c0b150ef298caa8b7a42bd4cab0d16f221a
PauMarquez99 <pauilimq99@gmail.com>
2022-11-25 19:06:02 -0600

    seguimientoPeticiones

---
+++ src/main/peticiones/ticket/submitDetalles.php
@@ -0,0 +1,16 @@
+<?php
+
+    require("../../conexionBD.php");
+
+    $query = "SELECT p.id, nominaSolicitante, s.nombre, s.correo, s.telefono, titulo, departamento, s.campus, poblacion, campusConsulta, nivelAcademico, periodos, camposInformacion, condiciones, uso, comentarios, estado, asignado, o.opeMail, fechaAlta, asignadaPor, fechaEntrega, fechaAsignacion, fechaTerminacion FROM peticiones p, solicitantes s, operarios o WHERE p.nominaSolicitante = s.nomina AND p.asignado=o.nomina AND id=1;
+    $result = consultaBD($query);
+
+    if(!$result) 
+        die(mysqli_error());
+
+
+$id=$_POST["detalles"];
+
+echo $id;
+
+?>
