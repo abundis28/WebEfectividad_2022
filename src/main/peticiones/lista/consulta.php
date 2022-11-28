8bd6c773b8559dabace187ee11505add426a8f3d
DRogerSC <musicfan211@hotmail.com>
2022-11-28 15:27:01 -0600

    'Changes to use global toke in query string'

--- src/main/peticiones/lista/consulta.php
+++ src/main/peticiones/lista/consulta.php
@@ -1,13 +1,31 @@
 <?php
     require("../../conexionBD.php");
 
-    $query = "SELECT id, nombre, fechaAlta, fechaAsignacion, fechaTerminacion, estado, operador, fechaEntrega FROM peticiones p, solicitantes s, operarios o WHERE p.nominaSolicitante = s.nomina AND p.asignado=o.nomina ORDER BY id";
+    
+
+   $username = $_SESSION['usuario'];
+
+   echo '<script>console.log("User is done!"); </script>';
+
+    $query = "SELECT id, nombre, fechaAlta, fechaAsignacion, fechaTerminacion, estado, operador, fechaEntrega 
+    FROM peticiones p, solicitantes s, operarios o 
+    WHERE p.nominaSolicitante = s.nomina AND p.asignado=o.nomina
+
+    ORDER BY id";
     $result = consultaBD($query);
 
+    //
+    echo '<script>console.log("Query was executed!"); </script>';
+
+    //WHERE s.username = "$username" AND p.nominaSolicitante = s.nomina
+    //WHERE s.username = \"$username\" AND 
     if(!$result) 
         die(mysqli_error());
 
     while($row = mysqli_fetch_array($result)) {
         echo '<tr><td>'.$row['id'].'</td><td>'.$row['nombre'].'</td><td>'.$row['fechaAlta'].'</td><td>'.$row['fechaAsignacion'].'</td><td>'.$row['fechaTerminacion'].'</td><td>'.$row['estado'].'</td><td>'.$row['operador'].'</td><td>'.$row['fechaEntrega'].'</td><td><form id=detalles action=../ticket/submitDetalles.php method=post><button id=detalles name=detalles type=submit value='.$row['id'].' class=detalles/>Detalles</button></form></td></tr>';
     }
+
+    
+    
 ?>
