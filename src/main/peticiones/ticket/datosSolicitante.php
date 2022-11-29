63a15a677d0086f6e6711a45cbb7af64fe634e0c
DRogerSC <musicfan211@hotmail.com>
2022-11-29 16:30:46 -0600

    Revert "Revert "Detalles ya muestran usuarios correctos""
    This reverts commit fefd8320554d5c0bc4e7a92bcb7cb1e057f353fe.

--- src/main/peticiones/ticket/datosSolicitante.php
+++ src/main/peticiones/ticket/datosSolicitante.php
@@ -1,7 +1,13 @@
 <?php
     require("../../conexionBD.php");
+
+    $id = $_SESSION['id'];
+
     $user = $_SESSION['usuario'];
-    $query = "SELECT nomina, nombre, correo, telefono, titulo, departamento, campus FROM solicitantes WHERE username = '$user'";
+    $query = "SELECT nomina, nombre, correo, telefono, titulo, departamento, campus 
+    FROM solicitantes s, peticiones p
+    WHERE s.nomina = p.nominaSolicitante
+    AND p.id = '$id'";
     $result = consultaBD($query);
 
     if(!$result) 
